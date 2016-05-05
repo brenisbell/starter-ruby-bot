f60827201ff9e024e9cb852f32f080abb2f4c07d
brenisbell <bren.isbell@thesupergroup.com>
2016-05-05 12:49:55 -0400

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -1,111 +1,37 @@
 require 'slack-ruby-client'
-require 'logging'
-
-logger = Logging.logger(STDOUT)
-logger.level = :debug
 
 Slack.configure do |config|
-  config.token = ENV['SLACK_TOKEN']
-  if not config.token
-    logger.fatal('Missing ENV[SLACK_TOKEN]! Exiting program')
-    exit
-  end
+  config.token = ENV['SLACK_API_TOKEN']
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger::INFO
+  fail 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 end
 
 client = Slack::RealTime::Client.new
 
-# listen for hello (connection) event - https://api.slack.com/events/hello
 client.on :hello do
-  logger.debug("Connected '#{client.self['name']}' to '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com.")
+  puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
 end
 
-# listen for channel_joined event - https://api.slack.com/events/channel_joined
-client.on :channel_joined do |data|
-  if joiner_is_bot?(client, data)
-    client.message channel: data['channel']['id'], text: "Thanks for the invite! I don\'t do much yet, but #{help}"
-    logger.debug("#{client.self['name']} joined channel #{data['channel']['id']}")
-  else
-    logger.debug("Someone far less important than #{client.self['name']} joined #{data['channel']['id']}")
-  end
-end
-
-# listen for channel_joined event - https://api.slack.com/events/channel_joined
-client.on :hello do
-  puts "Hello <@#{data['user']}>"
-end
-
-# listen for message event - https://api.slack.com/events/message
 client.on :message do |data|
+  puts data
 
-  case data['text']
-  when 'shit', 'fuck' then
-    client.typing channel: data['channel']
-    client.message channel: data['channel'], text: "Damn <@#{data['user']}>."
-    logger.debug("<@#{data['user']}> said hi")
+  client.typing channel: data.channel
 
-    if direct_message?(data)
-      client.message channel: data['channel'], text: "It\'s nice to talk to you directly."
-      logger.debug("And it was a direct message")
-    end
-
-  when 'attachment', 'bot attachment', ':taco:', '<@toddlerchris>:taco:' then
-    # attachment messages require using web_client
-    client.web_client.chat_postMessage(post_message_payload(data))
-    logger.debug("Attachment message posted")
-
-  when bot_mentioned(client)
-    client.message channel: data['channel'], text: 'You really do care about me. :heart:'
-    logger.debug("Bot mentioned in channel #{data['channel']}")
-
-  when 'bot help', 'help' then
-    client.message channel: data['channel'], text: help
-    logger.debug("A call for help")
-
+  case data.text
+  when 'bot hi' then
+    client.message channel: data.channel, text: "Hi <@#{data.user}>!"
   when /^bot/ then
-    client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, I don\'t understand. \n#{help}"
-    logger.debug("Unknown command")
+    client.message channel: data.channel, text: "Sorry <@#{data.user}>, what?"
   end
 end
 
-def direct_message?(data)
-  # direct message channles start with a 'D'
-  data['channel'][0] == 'D'
+client.on :close do |_data|
+  puts 'Connection closing, exiting.'
 end
 
-def bot_mentioned(client)
-  # match on any instances of `<@bot_id>` in the message
-  /\<\@#{client.self['id']}\>+/
-end
-
-def joiner_is_bot?(client, data)
- /^\<\@#{client.self['id']}\>/.match data['channel']['latest']['text']
-end
-
-def help
-  %Q(I will respond to the following messages: \n
-      `bot hi` for a simple message.\n
-      `bot attachment` to see a Slack attachment message.\n
-      `@<your bot\'s name>` to demonstrate detecting a mention.\n
-      `bot help` to see this again.)
-end
-
-def post_message_payload(data)
-  main_msg = 'help me'
-  {
-    channel: data['channel'],
-      as_user: true,
-      attachments: [
-        {
-          fallback: main_msg,
-          pretext: ':sunglasses: :thumbsup:',
-          title: 'meemee',
-          image_url: 'http://i.imgur.com/mg8SjSt.jpg',
-          title_link: 'dang',
-          text: main_msg,
-          color: '#7CD197'
-        }
-      ]
-  }
+client.on :closed do |_data|
+  puts 'Connection has been disconnected.'
 end
 
 client.start!
