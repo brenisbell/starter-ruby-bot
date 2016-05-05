4d2b23e7bb00684d2851f21ce1ba66c7d679ede8
brenisbell <bren.isbell@thesupergroup.com>
2016-05-05 13:31:20 -0400

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -16,7 +16,7 @@ client = Slack::RealTime::Client.new
 
 # listen for hello (connection) event - https://api.slack.com/events/hello
 client.on :hello do
-  client.web_client.chat_postMessage(post_message_payload2(data))
+  client.message channel: data['channel']['id'], text: "hellow"
     logger.debug("hello")
 end
 
@@ -93,25 +93,6 @@ end
 
 def post_message_payload(data)
   main_msg = 'help me'
-  {
-    channel: data['channel'],
-      as_user: true,
-      attachments: [
-        {
-          fallback: main_msg,
-          pretext: ':sunglasses: :thumbsup:',
-          title: 'meemee',
-          image_url: 'http://i.imgur.com/mg8SjSt.jpg',
-          title_link: 'dang',
-          text: main_msg,
-          color: '#7CD197'
-        }
-      ]
-  }
-end
-
-def post_message_payload2(data)
-  main_msg = 'hello again'
   {
     channel: data['channel'],
       as_user: true,
